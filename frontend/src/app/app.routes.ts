00647c0aae84a196675bfb5f3c1c1f4ca76729f1
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2025-04-27 15:06:36 +0300

    [FE]: routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { adminGuard } from './guards/admin.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
+  { path: 'register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
+  
+  { path: 'movies', loadComponent: () => import('./pages/movies/movie-list/movie-list.component').then(m => m.MovieListComponent) },
+  { path: 'movies/:id', loadComponent: () => import('./pages/movies/movie-detail/movie-detail.component').then(m => m.MovieDetailComponent) },
+  
+  { 
+    path: 'admin/movies/create', 
+    loadComponent: () => import('./pages/movies/movie-form/movie-form.component').then(m => m.MovieFormComponent),
+    canActivate: [authGuard, adminGuard]
+  },
+  { 
+    path: 'admin/movies/:id/edit', 
+    loadComponent: () => import('./pages/movies/movie-form/movie-form.component').then(m => m.MovieFormComponent),
+    canActivate: [authGuard, adminGuard]
+  },
+  
+  { path: '**', redirectTo: '' }
+];

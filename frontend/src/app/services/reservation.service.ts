f419aacd678a6897bd8bd00907164633a8d377ef
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2025-04-27 14:56:14 +0300

    [FE]: reservation service

---
+++ frontend/src/app/services/reservation.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Reservation {
+  id: number;
+  userId: number;
+  showingId: number;
+  seat: number;
+  movieName?: string;
+  theaterName?: string;
+  startTime?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+  private apiUrl = 'http://localhost:3000/reservations';
+
+  constructor(private http: HttpClient) {}
+
+  getReservations(): Observable<Reservation[]> {
+    return this.http.get<Reservation[]>(this.apiUrl);
+  }
+
+  createReservation(showingId: number, seat: number): Observable<Reservation> {
+    return this.http.post<Reservation>(this.apiUrl, { showingId, seat });
+  }
+
+  cancelReservation(id: number): Observable<{success: boolean}> {
+    return this.http.delete<{success: boolean}>(`${this.apiUrl}/${id}`);
+  }
+}

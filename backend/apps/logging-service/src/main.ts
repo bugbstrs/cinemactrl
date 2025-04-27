4c01f7ad3568887677281d9f0ae51f31449bfd5b
Stoica-David <stoicadavidioan@gmail.com>
2025-04-27 16:32:28 +0300

    [BE] Added Swagger and removed test file for logging service app in backend.

--- backend/apps/logging-service/src/main.ts
+++ backend/apps/logging-service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { LoggingServiceModule } from './logging-service.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(LoggingServiceModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Cinema Management API')
+      .setDescription('API for Cinema Management')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();

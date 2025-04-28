5156556e0f60d5ae8ff86b82a7b56559e3dcb58f
Vlad-Stefan Sas <me@vlad.pm>
2025-04-28 07:51:37 +0300

    fix: (FE) missing routes + small changes

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 
 import Aura from '@primeng/themes/aura';
@@ -15,6 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withInterceptors([authInterceptor])
     ),
+    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -20,6 +20,45 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/movies/movie-form/movie-form.component').then(m => m.MovieFormComponent),
     canActivate: [authGuard, adminGuard]
   },
-  
+  {
+    path: 'admin/theaters',
+    loadComponent: () => import('./pages/theaters/theater-list/theater-list.component').then(m => m.TheaterListComponent),
+    canActivate: [authGuard, adminGuard]
+  },
+  {
+    path: 'admin/theaters/:id/edit',
+    loadComponent: () => import('./pages/theaters/theater-form/theater-form.component').then(m => m.TheaterFormComponent),
+    canActivate: [authGuard, adminGuard]
+  },
+  {
+    path: 'admin/theaters/create',
+    loadComponent: () => import('./pages/theaters/theater-form/theater-form.component').then(m => m.TheaterFormComponent),
+    canActivate: [authGuard, adminGuard]
+  },
+  {
+    path: 'showings',
+    loadComponent: () => import('./pages/showings/showing-list/showing-list.component').then(m => m.ShowingListComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'showings/:id',
+    loadComponent: () => import('./pages/showings/showing-detail/showing-detail.component').then(m => m.ShowingDetailComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'admin/showings/:id/edit',
+    loadComponent: () => import('./pages/showings/showing-form/showing-form.component').then(m => m.ShowingFormComponent),
+    canActivate: [authGuard, adminGuard]
+  },
+  {
+    path: 'admin/showings/create',
+    loadComponent: () => import('./pages/showings/showing-form/showing-form.component').then(m => m.ShowingFormComponent),
+    canActivate: [authGuard, adminGuard]
+  },
+  {
+    path: 'reservations',
+    loadComponent: () => import('./pages/reservations/reservation-list/reservation-list.component').then(m => m.ReservationListComponent),
+    canActivate: [authGuard]
+  },
   { path: '**', redirectTo: '' }
 ];
